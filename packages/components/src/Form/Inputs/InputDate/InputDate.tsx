553fa6d57ee485ed6311a3affd546440fd31c934
Daniel Christopher <dbchristopher@gmail.com>
2020-01-28 09:41:33 -0800

    render react-day-picker

--- packages/components/src/Form/Inputs/InputDate/InputDate.tsx
+++ packages/components/src/Form/Inputs/InputDate/InputDate.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react'
+import DayPicker from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
 
 interface InputDateProps {
   prop?: any
 }
 
 export const InputDate: FC<InputDateProps> = () => {
-  return <div>INPUT DATE</div>
+  return <DayPicker />
 }
